7a3fd09d18a29da8d1d98c32c0a5925f1faf4e71
hernanchoquehuanca <hchoquehuancaz@unsa.edu.pe>
2024-05-22 21:45:00 -0500

    Implementando el metodo horizontalRepeat

--- picture.py
+++ picture.py
@@ -51,7 +51,10 @@ class Picture:
     def horizontalRepeat(self, n):
         """ Devuelve una nueva figura repitiendo la figura actual al costado
             la cantidad de veces que indique el valor de n """
-        return Picture(None)
+        if n < 1:
+            return Picture(self.img)
+        new_img = [line * n for line in self.img]
+        return Picture(new_img)
 
     def verticalRepeat(self, n):
         return Picture(None)
